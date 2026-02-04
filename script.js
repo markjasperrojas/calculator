dd20d2cce5de77c0faa5a26caede7be123055845
markjasperrojas <markjasperrojas1@gmail.com>
2026-02-04 22:30:22 +0800

    Add clear & delete functionality

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-let screen = document.getElementById("screen");
+function deleteHandle() {
+  let newValue = screen.value.split("");
+  newValue.pop();
+  screen.value = newValue.join("");
+}
+
+const screen = document.getElementById("screen");
 const buttons = document.querySelectorAll("button");
+const clearBtn = document.getElementById("clear");
+const deleteBtn = document.getElementById("delete");
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     screen.value += button.id;
   });
 });
+
+clearBtn.addEventListener("click", () => {
+  screen.value = "";
+});
+
+deleteBtn.addEventListener("click", deleteHandle);
