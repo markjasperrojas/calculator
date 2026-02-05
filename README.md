9d6bde6bd24d09f3caa025b002e45326ea2da971
markjasperrojas <markjasperrojas1@gmail.com>
2026-02-06 02:26:46 +0800

    Finish basic functions

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # calculator
+
+I just finished the calculator learning project from [The Odin Project](https://www.theodinproject.com/). This calculator only has basic functions, no design yet, and the code is only about 25% to 40% optimized based on my estimate. You may still see some small bugs, and I will fix them soon.
+
+The hardest part of this project is that using eval() or similar methods is not allowed, so you really need to handle the logic yourself. Using AI is also not allowed. I did not fully polish the code and UI since this is my second time making a calculator.
+
+That’s all for now. I am moving forward to the Full Stack JavaScript path.
--- script.js
+++ script.js
@@ -1,22 +1,149 @@
-function deleteHandle() {
-  let newValue = screen.value.split("");
-  newValue.pop();
-  screen.value = newValue.join("");
+function reset() {
+  firstNumber = "";
+  secondNumber = "";
+  operator = "";
+  hasOperator = false;
+}
+
+function deleteHandle(str) {
+  let newStr = str.split("");
+  newStr.pop();
+  str = newStr.join("");
+  return str;
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(a, b, operator) {
+  return operator === "+"
+    ? add(a, b)
+    : operator === "-"
+      ? subtract(a, b)
+      : operator === "x"
+        ? multiply(a, b)
+        : divide(a, b);
+}
+
+function secondOperate() {
+  if (operator === "/" && firstNumber === "0" && secondNumber === "0") {
+    screen.value = "Result is undefined";
+    reset();
+    return;
+  }
+
+  if (operator === "/" && secondNumber === "0") {
+    screen.value = "Cannot divide by 0";
+    reset();
+    return;
+  }
+
+  firstNumber = Number(
+    operate(Number(firstNumber), Number(secondNumber), operator).toFixed(3),
+  ).toString();
+  secondNumber = "";
+  screen.value = firstNumber;
 }
 
 const screen = document.getElementById("screen");
 const buttons = document.querySelectorAll("button");
 const clearBtn = document.getElementById("clear");
 const deleteBtn = document.getElementById("delete");
+const operators = document.querySelectorAll(".operator");
+const equalBtn = document.getElementById("equal");
+const dotBtn = document.getElementById("dot");
+
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
+let hasOperator = false;
+let hasDot = false;
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    screen.value += button.id;
+    if (!hasOperator) {
+      firstNumber += button.id;
+      screen.value = firstNumber;
+    } else if (hasOperator) {
+      secondNumber += button.id;
+      screen.value = secondNumber;
+    }
   });
 });
 
 clearBtn.addEventListener("click", () => {
+  reset();
   screen.value = "";
 });
 
-deleteBtn.addEventListener("click", deleteHandle);
+deleteBtn.addEventListener("click", () => {
+  if (!hasOperator) {
+    firstNumber = deleteHandle(firstNumber);
+    screen.value = firstNumber;
+  } else if (hasOperator) {
+    secondNumber = deleteHandle(secondNumber);
+    screen.value = secondNumber;
+  }
+});
+
+operators.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (
+      firstNumber.length !== 0 &&
+      secondNumber.length !== 0 &&
+      operator.length !== 0
+    ) {
+      secondOperate();
+      operator = button.value;
+    }
+
+    if (hasOperator || firstNumber.length === 0) return;
+
+    hasOperator = true;
+    operator = button.value;
+  });
+});
+
+equalBtn.addEventListener("click", () => {
+  if (
+    firstNumber.length === 0 ||
+    secondNumber.length === 0 ||
+    operator.length === 0
+  )
+    return;
+
+  hasOperator = false;
+  secondOperate();
+  reset();
+});
+
+dotBtn.addEventListener("click", () => {
+  if (!hasOperator && firstNumber.length === 0) {
+    firstNumber += "0.";
+    screen.value = firstNumber;
+  } else if (hasOperator && secondNumber.length === 0) {
+    secondNumber += "0.";
+    screen.value = secondNumber;
+  }
+
+  if (!hasOperator && !firstNumber.includes(".")) {
+    firstNumber += ".";
+    screen.value = firstNumber;
+  } else if (hasOperator && !secondNumber.includes(".")) {
+    secondNumber += ".";
+    screen.value = secondNumber;
+  }
+});
